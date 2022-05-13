1cbbb7714c5c9fe18d9b8a139bd7fa8a3c6e7ed1
docluis <42699881+docluis@users.noreply.github.com>
2022-05-13 14:39:17 +0200

    added side-bar

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,46 @@ import {
   Code,
   Grid,
   theme,
+  Flex,
+  Center,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import Sidebar from "./components/Sidebar"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Flex>
+      <Sidebar />
+
+      <Center textAlign="center" fontSize="xl"
+        height="40px"
+        width="40px"
+        position="absolute"
+        top="50%"
+        left="50%"
+        transform="translateY(-50%, -50%)"
+      >
+        <Grid minH="100vh" p={300}>
+          
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Center>
+
+    </Flex>
+    
   </ChakraProvider>
 )
---
+++ src/components/Sidebar.js
@@ -0,0 +1,100 @@
+// Sidebar Base by Benjamin Carlson
+// https://github.com/bjcarlson42/chakra-left-responsive-navbar
+// 12.05.2022
+
+
+import React, { useState } from 'react'
+import {
+    Flex,
+    Text,
+    IconButton,
+    Divider,
+    Avatar,
+    Heading
+} from '@chakra-ui/react'
+import {
+    FiMenu,
+    FiHome,
+    FiCalendar,
+    FiBookOpen,
+    FiArrowUpCircle,
+    FiShield,
+    FiSettings
+} from 'react-icons/fi'
+import NavItem from '../components/NavItem'
+import { ColorModeSwitcher } from "../components/ColorModeSwitcher"
+
+export default function Sidebar() {
+    const [navSize, changeNavSize] = useState("large")
+    return (
+        <Flex
+            pos="sticky"
+            left="5"
+            h="95vh"
+            marginTop="2.5vh"
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+            borderRadius={navSize === "small" ? "15px" : "30px"}
+            w={navSize === "small" ? "75px" : "200px"}
+            flexDir="column"
+            justifyContent="space-between"
+        >
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize === "small" ? "center" : "flex-start"}
+                as="nav"
+            >
+                <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={<FiMenu />}
+                    onClick={() => {
+                        if (navSize === "small")
+                            changeNavSize("large")
+                        else
+                            changeNavSize("small")
+                    }}
+                />
+                
+                <NavItem navSize={navSize} icon={FiHome} title="Home" description="This is the description for the dashboard." active/>
+                <NavItem navSize={navSize} icon={FiCalendar} title="Calendar" />
+                <NavItem navSize={navSize} icon={FiBookOpen} title="Wiki" />
+                <NavItem navSize={navSize} icon={FiArrowUpCircle} title="Reinforcer" />
+                <NavItem navSize={navSize} icon={FiShield} title="Emergency" />
+                <NavItem navSize={navSize} icon={FiSettings} title="Settings" />
+            </Flex>
+
+            
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize === "small" ? "center" : "flex-start"}
+                mb={4}
+            >
+                <ColorModeSwitcher justifySelf="flex-end" />
+            </Flex>
+
+
+
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize === "small" ? "center" : "flex-start"}
+                mb={4}
+            >
+                <Divider display={navSize === "small" ? "none" : "flex"} />
+                <Flex mt={4} align="center">
+                    <Avatar size="sm" src="avatar-1.jpg" />
+                    <Flex flexDir="column" ml={4} display={navSize === "small" ? "none" : "flex"}>
+                        <Heading as="h3" size="sm">Max Musterman</Heading>
+                        <Text color="gray">Username</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Flex>
+    )
+}
