248064fe255ddbd607b0748d64d86f596056235d
enese43 <enes.erdem.43@gmail.com>
2022-05-23 11:35:26 +0200

    new feature

---
+++ src/routes/safetyNet.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
+    Flex,
+    Center,
+    Button,
+    Spacer,
+    Heading,
+} from "@chakra-ui/react"
+import Sidebar from "../components/Sidebar"
