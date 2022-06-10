2dae55df9e85d26e73a346a92a64564ef89211eb
docluis <42699881+docluis@users.noreply.github.com>
2022-06-10 12:56:47 +0200

    favicon, home page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import {
   theme,
   Flex,
   Center,
+  Heading,
+  Image,
 } from "@chakra-ui/react"
 // import { Logo } from "./Logo"
 import Sidebar from "./components/Sidebar"
@@ -28,22 +30,11 @@ export const App = () => (
                     transform="translate(-50%, -10%)"
                     maxWidth='800px'
                 >
-        <Grid minH="50vh" p={300}>
-          
+        <Grid minH="50vh" p={200}>
           <VStack spacing={8}>
             {/* <Logo h="40vmin" pointerEvents="none" /> */}
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Image src='/header.png' />
+            <Text fontSize="20">Welcome to the Kopsachen Web App!</Text>
           </VStack>
         </Grid>
       </Flex>
