808361473b3891d1c7ce5c5dd707dfa7c84c7faa
docluis <42699881+docluis@users.noreply.github.com>
2022-06-20 17:42:45 +0200

    Revert "feature-reframingv1"

--- src/routes/Reframing1.tsx
+++ src/routes/Reframing1.tsx
@@ -18,33 +18,23 @@ import {
 } from "@chakra-ui/react";
 
 import { useNavigate } from "react-router-dom";
-import Sidebar from "../components/Sidebar"
-import { AspectRatio } from '@chakra-ui/react'
-
-import { useForm, SubmitHandler } from "react-hook-form";
-import { stringify } from "querystring"
-import { networkInterfaces } from "os"
-import axios from "axios"
-
-interface IReframingItem {
-    situations: string[];
-}
+import Sidebar from "../components/Sidebar";
+import { AspectRatio } from "@chakra-ui/react";
 
 export default function New() {
   const navigate = useNavigate();
 
-        const { register, handleSubmit, setValue } = useForm<IReframingItem>();
-        const onSubmit: SubmitHandler<IReframingItem> = data => axios.post(`http://127.0.0.1:4010/safetyNet/1`, data)
-        
-
-    return (
-        <ChakraProvider theme={theme}>
-            <Flex>
-                <Sidebar />
-                <Button  marginLeft={270} colorScheme='teal' variant='ghost' onClick={() => navigate
-                    ('/newresources')}>
-                        ← Back 
-                </Button>
+  return (
+    <Flex>
+      <Sidebar />
+      <Button
+        marginLeft={270}
+        colorScheme="teal"
+        variant="ghost"
+        onClick={() => navigate("/newresources")}
+      >
+        ← Back
+      </Button>
 
       <Flex
         flexDirection="column"
@@ -63,57 +53,36 @@ export default function New() {
             </Heading>
           </Center>
 
-                        <Stack>
-                            <Center>
-                                <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}  >
-                                    <Text color={'green.400'} pt={'50px'}  >
-                                
-                                        Reframing
-                                
-                                    </Text>
-                                </Heading>
-                                </Center>
-                                
-                            <Stack>
-                                <Center>
-                                    <Text fontSize={{ base: 'md', lg: '2xl' }} color={'white'} pt={'50px'} >
-                                        Which Situation is bothering you at the moment? {'\n'}
-                                        Maybe there is more than one, we are going to go through each situaion step by step.
-                                    </Text>
-                                </Center>
-                            </Stack>
-                        
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                            <Stack spacing={3} pt={'1px'}>
+          <Stack>
+            <Center>
+              <Text
+                fontSize={{ base: "md", lg: "2xl" }}
+                color={"white"}
+                pt={"50px"}
+              >
+                Which Situation is bothering you at the moment? {"\n"}
+                Maybe there is more than one, we are going to go through each
+                situaion step by step.
+              </Text>
+            </Center>
+          </Stack>
 
-                                <Input {...register(`situations.${0}`)} placeholder='Situation 1' size='lg' />
-                                <Input {...register(`situations.${1}`)} placeholder='Situation 2' size='lg' />
-                                <Input {...register(`situations.${2}`)} placeholder='Situation 3' size='lg' />
-                                <Input {...register(`situations.${3}`)} placeholder='Situation 4' size='lg' />
+          <Stack spacing={3} pt={"1px"}>
+            <Input placeholder="Situation 1" size="lg" />
+            <Input placeholder="Situation 2" size="lg" />
+            <Input placeholder="Situation 3" size="lg" />
+            <Input placeholder="Situation 4" size="lg" />
+          </Stack>
 
-                            </Stack>
-                            
-        
-        <Center >
-            
-        <Stack direction='row' spacing={20}  pt={'50px'}>
-    <Button colorScheme='teal' variant='solid' size='lg' type='submit' onClick={()=>navigate('/Reframing2')}>
-        These are all the situation that are bothering me at the moment
-
-    </Button>
-    
-        
-    </Stack>
-        </Center>
-        </form>
-
-</Stack>
-
-            </Flex>
-            </Flex>  
-            
-        </ChakraProvider>
-    );
-    }
-
-
+          <Center>
+            <Stack direction="row" spacing={20} pt={"50px"}>
+              <Button colorScheme="teal" variant="solid" size="lg">
+                These are all the situation that are bothering me at the moment
+              </Button>
+            </Stack>
+          </Center>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+}
