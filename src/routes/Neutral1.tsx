47753c2e78b8117d446bfac4eff423e351375cd9
enese43 <enes.erdem.43@gmail.com>
2022-07-03 15:17:03 +0200

    Done button functionality added

--- src/routes/Neutral1.tsx
+++ src/routes/Neutral1.tsx
@@ -5,6 +5,7 @@ import {
 import Sidebar from "../components/Sidebar"
 
 import {translationsEn, translationsTr, translationsDe, translationsAl} from "../components/translationText"
+import { useNavigate } from "react-router-dom";
 
 
 import { useForm, SubmitHandler } from "react-hook-form";
@@ -40,7 +41,7 @@ interface ISafteyNetItem {
 export default function Neutral1(){
 
     const [isText, setIsText] = useState(false);
-
+    const navigate = useNavigate();
     const [show, setShow] = useState(false);
 
     const [isFertigButton, setIsFertigButton] = useState(false);
@@ -51,6 +52,10 @@ export default function Neutral1(){
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { isOpen: isOpen1, onOpen: onOpen1, onClose: onClose1} = useDisclosure()
     const { isOpen: isOpen2, onOpen: onOpen2, onClose: onClose2} = useDisclosure()
+    const { isOpen: isOpenFeedback, onOpen: onOpenFeedback, onClose: onCloseFeedback} = useDisclosure()
+  const onModalClose = () => {
+    onCloseFeedback();
+  };
 
     const [nameInput1, setNameInput1] = useState<string[]>([]);
     const [nameInput2, setNameInput2] = useState<string>('');
@@ -77,7 +82,7 @@ export default function Neutral1(){
         </Center>
         <Text paddingLeft={"20%"} maxW={"90%"}>Um dir diesen Starkmacher genauer anzuschauen, überlege dir erst einmal welche Menschen es überhaupt alles in deinem Umfeld gibt – immerhin, wer weiß, wer von ihnen dir in Zukunft vielleicht einmal in irgendeiner Form unterstützend zur Seite stehen kann? Skizziere dazu dein persönliches Netzwerk in folgendem Schema mit dir selbst als gedachtem Mittelpunkt:</Text>
           <br></br>
-        <Text paddingLeft={"20%"} maxW={"90%"}>Der innere Kreis – Trage hier die wichtigsten Menschen in deinem Leben ein. Der mittlere Kreis – Trage hier Personen ein, die dir noch nahe stehen und/oder mit denen du regelmäßig Zeit verbringst. Der äußere Kreis – Trage hier Bekannte und eher entferntere Personen ein, mit denen du aber auch ab und zu zu tun hast. Klicke einfach auf das plus-Symbol, um Personen dem jeweiligen Kreis hinzuzufügen.</Text>
+        <Text paddingLeft={"20%"} maxW={"90%"}><span className="innereText"><b>Der innere Kreis</b></span> – Trage hier die wichtigsten Menschen in deinem Leben ein. <span className="mittlereText"><b>Der mittlere Kreis</b></span> – Trage hier Personen ein, die dir noch nahe stehen und/oder mit denen du regelmäßig Zeit verbringst. <span className="ausereText"><b>Der äußere Kreis</b></span> – Trage hier Bekannte und eher entferntere Personen ein, mit denen du aber auch ab und zu zu tun hast. Klicke einfach auf das plus-Symbol, um Personen dem jeweiligen Kreis hinzuzufügen.</Text>
         <br></br>
           
         <Box className="container" paddingLeft={"40%"}>
@@ -229,7 +234,60 @@ export default function Neutral1(){
                     
         </Box>    
         
-        {isFertigButton && <Button width={'140px'} backgroundColor={'#F3803E'} marginTop={'20px'} marginLeft={'45.5%'}>Done!</Button>}  
+        {isFertigButton && <Button width={'140px'} backgroundColor={'#F3803E'} marginTop={'20px'} marginLeft={'45.5%'} onClick={onOpenFeedback} >Done!</Button>}  
+        <Modal isOpen={isOpenFeedback} onClose={onModalClose}>
+            <ModalOverlay>
+              <ModalContent>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <ModalHeader fontSize="lg" fontWeight="bold">
+                    Feedback
+                  </ModalHeader>
+
+                  <ModalBody>
+                    <Stack direction="row" spacing={3}>
+                      <IconButton
+                        onClick={() => {
+                        //   setValue(`feedback.${0}.itHelped`, true);
+                        //   setItHelped(true);
+                        }}
+                        aria-label="positive"
+                        // variant={itHelped === true ? "solid" : "ghost"}
+                        icon={<FiSmile size={30} color="green" />}
+                      />
+                      <IconButton
+                        onClick={() => {
+                        //   setValue(`feedback.${0}.itHelped`, false);
+                        //   setItHelped(false);
+                        }}
+                        aria-label="negative"
+                        // variant={itHelped === false ? "solid" : "ghost"}
+                        icon={<FiFrown size={30} color="red" />}
+                      />
+                    </Stack>
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button
+                      onClick={() => {
+                        onModalClose();
+                        // prevent react from clearing the inputs before the form submit is handled
+                        setTimeout(() => {
+                        //   clearInputs();
+                          navigate("/mooddiary");
+                        }, 0);
+                      }}
+                      type="submit"
+                    //   isDisabled={typeof itHelped === "undefined"}
+                      mr={3}
+                    >
+                      Submit
+                    </Button>
+                    <Button onClick={onModalClose}>Cancel</Button>
+                  </ModalFooter>
+                </form>
+              </ModalContent>
+            </ModalOverlay>
+          </Modal>
         </Box>
         </Box>
         </Flex>
