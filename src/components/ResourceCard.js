55e6b1de35bb40b560e92280760edb9b52dff31c
docluis <42699881+docluis@users.noreply.github.com>
2022-07-07 13:11:35 +0200

    resource page redone

---
+++ src/components/ResourceCard.js
@@ -0,0 +1,101 @@
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function ResourceCard({ title, emoji, emojiaria, link, image }) {
+  return (
+    <Link to={link}>
+      <Center py={12}>
+        <Box
+          role={"group"}
+          p={6}
+          maxW={"330px"}
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          boxShadow={"2xl"}
+          rounded={"lg"}
+          pos={"relative"}
+          zIndex={1}
+        >
+          {/* <Box
+          rounded={"lg"}
+          mt={-12}
+          pos={"relative"}
+          height={"230px"}
+          _after={{
+            transition: "all .3s ease",
+            content: '""',
+            w: "full",
+            h: "full",
+            pos: "absolute",
+            top: 5,
+            left: 0,
+            // backgroundImage: `url(${image})`,
+            filter: "blur(15px)",
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: "blur(20px)",
+            },
+          }}
+        >
+          <Image
+            rounded={"lg"}
+            height={230}
+            width={282}
+            objectFit={"cover"}
+            src={image}
+          />
+        </Box> */}
+          <Box
+            rounded={"lg"}
+            mt={-12}
+            pos={"relative"}
+            height={"230px"}
+            _after={{
+              transition: "all .3s ease",
+              content: '""',
+              w: "full",
+              h: "full",
+              pos: "absolute",
+              top: 5,
+              left: 0,
+              // backgroundImage: `url(${image})`,
+              filter: "blur(15px)",
+              zIndex: -1,
+            }}
+            _groupHover={{
+              _after: {
+                filter: "blur(20px)",
+              },
+            }}
+          >
+            <Text role="img" aria-label={emojiaria} fontSize="9xl">
+              {emoji}
+            </Text>
+          </Box>
+          <Stack pt={10} align={"center"}>
+            <Text
+              color={"gray.500"}
+              fontSize={"sm"}
+              textTransform={"uppercase"}
+            >
+              Exercise
+            </Text>
+            <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+              {title}
+            </Heading>
+          </Stack>
+        </Box>
+      </Center>
+    </Link>
+  );
+}
